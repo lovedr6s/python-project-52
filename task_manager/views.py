a3fa81a2587977e3a411799ac0be3d30192faa4a
lovedr6s <praense@icloud.com>
2025-05-04 08:17:13 +0300

    Changed directories

--- task_manager/views.py
+++ task_manager/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, redirect
 from django.views import View
 
 class IndexView(View):
-    def get(self, request):
+    def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
