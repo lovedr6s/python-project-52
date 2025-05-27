e8f0faa4e723e703b94f67a163d894737b24d006
lovedr6s <praense@icloud.com>
2025-05-27 12:44:12 +0300

    Shit

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,14 +1,16 @@
 from django import forms
 from .models import Task
-from labels.models import Label  # Предполагается, что модель Status находится в приложении statuses
-#удалить после создания модели Task и получать из statuses.models
+from labels.models import Label
+
+
 class TaskForm(forms.ModelForm):
     tags = forms.ModelMultipleChoiceField(
         queryset=Label.objects.all(),
-        widget=forms.CheckboxSelectMultiple,  # или forms.SelectMultiple для выпадающего списка
+        widget=forms.CheckboxSelectMultiple,
         required=False,
         label='Метки',
     )
+
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'assignee', 'tags']
@@ -18,12 +20,26 @@ class TaskForm(forms.ModelForm):
             'status': 'Статус',
             'assignee': 'Исполнитель',
         }
-    
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            widget = field.widget
+
+            if isinstance(widget, forms.CheckboxSelectMultiple):
+                # Bootstrap для чекбоксов можно добавить тут при желании
+                widget.attrs.update({'class': 'form-check-input'})
+            elif isinstance(widget, forms.Select):
+                widget.attrs.update({'class': 'form-select'})
+            else:
+                widget.attrs.update({'class': 'form-control'})
+
     def clean_name(self):
         name = self.cleaned_data.get('name')
         if Task.objects.filter(name=name).exclude(pk=self.instance.pk).exists():
             raise forms.ValidationError("Задача с таким именем уже существует.")
         return name
+
     def save(self, commit=True):
         task = super().save(commit=False)
         if commit:
