d3b766743943d9e50e147dde224e5151ad1e73a4
lovedr6s <praense@icloud.com>
2025-06-10 13:21:53 +0300

    changed imports

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from .views import (
-    TaskCreateView, TaskDeleteView, TaskDetailView, TaskListView, TaskUpdateView,
+    TaskCreateView,
+    TaskDeleteView,
+    TaskDetailView,
+    TaskListView,
+    TaskUpdateView,
 )
 
 urlpatterns = [
